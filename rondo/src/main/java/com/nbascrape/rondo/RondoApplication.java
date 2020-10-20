c876e8d24be3e9839f57c1ea93615f14c4067689
John DeRiggi <john.deriggi@gmail.com>
2020-10-19 20:39:54 -0700

    first commit

---
+++ rondo/src/main/java/com/nbascrape/rondo/RondoApplication.java
@@ -0,0 +1,13 @@
+package com.nbascrape.rondo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RondoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RondoApplication.class, args);
+	}
+
+}
---
+++ rondo/src/main/java/com/nbascrape/rondo/Scraper.java
@@ -0,0 +1,30 @@
+package com.nbascrape.rondo;
+
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+public class Scraper {
+
+    void fetchThings(String url) {
+    }
+
+    public static void main(String[] args) {
+        final String url = "https://stats.nba.com/players/bio/?Season=2019-20&SeasonType=Regular%20Season&sort=PLAYER_NAME&dir=1";
+
+        System.out.println("printing");
+        try {
+            var doc = Jsoup.connect(url).get();
+            String title = doc.title();
+            Elements nameTds = doc.getElementsByTag("tbody");
+            System.out.println(title);
+            System.out.println(nameTds.size());
+
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+}
